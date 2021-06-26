1d8c0c3ee3f9e8d2d71296b73f0af210bb46eda5
deds129 <nik.chudinov129@mail.ru>
2021-06-26 21:00:24 +0300

    add filled data.sql + PostgreSQL course + created relationships between tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,29 @@
 CREATE TABLE users(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    login VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    user_id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_login VARCHAR(50) NOT NULL,
+    user_password VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE chatrooms(
+    chatroom_id BIGSERIAL NOT NULL PRIMARY KEY,
+    chatroom_name VARCHAR(50) NOT NULL,
+    /*Chatroom owner*/
+    chatroom_owner_id INTEGER REFERENCES users(user_id)
+);
+
+CREATE TABLE user_chats(
+    chat_id INTEGER REFERENCES chatrooms(chatroom_id),
+    user_in_chat_id INTEGER REFERENCES users(user_id),
+    PRIMARY KEY(chat_id,user_in_chat_id)
+);
+
+CREATE TABLE messages(
+    message_id BIGSERIAL NOT NULL PRIMARY KEY,
+    /*Message author*/
+    message_author INTEGER REFERENCES users(user_id),
+     /*List of messages in a chatroom*/
+    message_room INTEGER REFERENCES chatrooms(chatroom_id),
+    message_text VARCHAR (350),
+    message_date DATE
+);
+
