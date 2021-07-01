2e7aecd3a6c195da50b01deceb3c415eb219389e
deds129 <nik.chudinov129@mail.ru>
2021-07-01 13:01:11 +0300

    check connection to DB

--- src/main/java/edu/chat/app/Program.java
+++ src/main/java/edu/chat/app/Program.java
@@ -1,13 +1,60 @@
 package edu.chat.app;
 
+import java.sql.*;
+
 /**
  * Hello world!
  *
  */
-public class Program
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Program {
+    /**
+     * JDBC Driver and database url
+     */
+    static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/chat_db";
+
+    /**
+     * USER and PASSWORD CONFIGURATION
+     */
+    static final String USER = "postgres";
+    static final String PASSWORD = "postgres";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            String SQL = null;
+            ResultSet resultSet = null;
+
+            SQL = "SELECT * FROM users";
+            preparedStatement = connection.prepareStatement(SQL);
+
+            resultSet = preparedStatement.executeQuery();
+            outRequestResult(resultSet);
+
+            preparedStatement.close();
+            connection.close();
+
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void outRequestResult(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            int id = resultSet.getInt("user_id");
+            String login = resultSet.getString("user_login");
+            String password = resultSet.getString("user_password");
+
+
+            System.out.println("\n================\n");
+            System.out.println("id: " + id);
+            System.out.println("login: " + login);
+            System.out.println("password: " + password);
+
+        }
     }
 }
