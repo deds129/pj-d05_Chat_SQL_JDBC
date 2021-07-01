774c60f35daefe84a1e1202031f75da457d70620
deds129 <nik.chudinov129@mail.ru>
2021-07-01 15:41:08 +0300

    add inserts to tables(not tested)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,17 +10,54 @@ insert into users (user_login, user_password) values ('qharradine8', 'Df5Vbya');
 insert into users (user_login, user_password) values ('mgraalman9', 'nom06wqZG');
 
 
-insert into chatrooms (chatroom_name) values ('chat #1');
-insert into chatrooms (chatroom_name) values ('chat #2');
-insert into chatrooms (chatroom_name) values ('chat #3');
-insert into chatrooms (chatroom_name) values ('chat #4');
-insert into chatrooms (chatroom_name) values ('chat #5');
-insert into chatrooms (chatroom_name) values ('chat #6');
-insert into chatrooms (chatroom_name) values ('chat #7');
-insert into chatrooms (chatroom_name) values ('chat #8');
-insert into chatrooms (chatroom_name) values ('chat #9');
-insert into chatrooms (chatroom_name) values ('chat #10');
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #1',1);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #2',2);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #3',3);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #4',6);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #5',1);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #6',1);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #7',1);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #8',1);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #9',1);
+insert into chatrooms (chatroom_name, chatroom_owner_id) values ('chat #10',10);
 
-insert into messages () values ();
+insert into user_chats(chat_id, user_in_chat_id)  values (2,1);
+insert into user_chats(chat_id, user_in_chat_id)  values (2,2);
 
+insert into user_chats(chat_id, user_in_chat_id)  values (1,1);
+insert into user_chats(chat_id, user_in_chat_id)  values (1,2);
+insert into user_chats(chat_id, user_in_chat_id)  values (1,3);
+insert into user_chats(chat_id, user_in_chat_id)  values (1,4);
+insert into user_chats(chat_id, user_in_chat_id)  values (1,5);
 
+insert into user_chats(chat_id, user_in_chat_id)  values (3,1);
+insert into user_chats(chat_id, user_in_chat_id)  values (3,2);
+insert into user_chats(chat_id, user_in_chat_id)  values (3,3);
+
+/*test*/
+insert into user_chats(chat_id, user_in_chat_id)  values (4,3);
+
+insert into messages (message_author, message_room,  message_text, message_date) values (1,2,'HELLO!', '3/11/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (1,2,'How are you?', '8/5/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (2,2,'Fine! Thx! What about u?', '6/5/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (2,2,'Where are u?', '8/10/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (1,2,'Ama here! I am good', '5/12/2021');
+
+insert into messages (message_author, message_room, message_text, message_date) values (1,1,'Enterprise-wide coherent local area network', '20/1/2020');
+insert into messages (message_author, message_room, message_text, message_date) values (2,1,'Cross-group 3rd generation algorithm', '12/1/2020');
+insert into messages (message_author, message_room, message_text, message_date) values (3,1,'Horizontal 3rd generation intranet', '5/1/2020');
+insert into messages (message_author, message_room, message_text, message_date) values (4,1,'Up-sized discrete open architecture', '5/1/2020');
+insert into messages (message_author, message_room, message_text, message_date) values (5,1,'Secured dedicated throughput', '6/1/2020');
+
+insert into messages (message_author, message_room, message_text, message_date) values (1,3,'Optimized interactive service-desk', '1/2/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (2,3,'Business-focused hybrid initiative', '1/2/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (3,3,'Digitized background policy', '8/3/2020');
+insert into messages (message_author, message_room, message_text, message_date) values (1,3,'Integrated systematic firmware', '3/4/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (2,3,'Progressive value-added secured line', '8/4/2020');
+
+/*test*/
+insert into messages (message_author, message_room, message_text, message_date) values (1,4,'Optimized coherent info-mediaries', '3/11/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (1,4,'Sharable motivating toolset', '11/11/2020');
+insert into messages (message_author, message_room, message_text, message_date) values (2,4,'Intuitive transitional emulation', '8/12/2020');
+insert into messages (message_author, message_room, message_text, message_date) values (3,4,'Sharable didactic local area network', '3/12/2021');
+insert into messages (message_author, message_room, message_text, message_date) values (5,4,'Distributed context-sensitive projection', '7/12/2020');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-/*CREATE DATABASE chat_db*/
+/*CREATE DATABASE chat_db; */
 
 CREATE TABLE users(
     user_id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -10,22 +10,22 @@ CREATE TABLE chatrooms(
     chatroom_id BIGSERIAL NOT NULL PRIMARY KEY,
     chatroom_name VARCHAR(50) NOT NULL,
     /*Chatroom owner*/
-    chatroom_owner_id INTEGER REFERENCES users(user_id)
+    chatroom_owner_id INTEGER REFERENCES users(user_id) NOT NULL
 );
 
 CREATE TABLE user_chats(
-    chat_id INTEGER REFERENCES chatrooms(chatroom_id),
-    user_in_chat_id INTEGER REFERENCES users(user_id),
+    chat_id INTEGER REFERENCES chatrooms(chatroom_id) NOT NULL,
+    user_in_chat_id INTEGER REFERENCES users(user_id) NOT NULL,
     PRIMARY KEY(chat_id,user_in_chat_id)
 );
 
 CREATE TABLE messages(
     message_id BIGSERIAL NOT NULL PRIMARY KEY,
     /*Message author*/
-    message_author INTEGER REFERENCES users(user_id),
+    message_author INTEGER REFERENCES users(user_id) NOT NULL,
      /*List of messages in a chatroom*/
-    message_room INTEGER REFERENCES chatrooms(chatroom_id),
-    message_text VARCHAR (350),
-    message_date DATE
+    message_room INTEGER REFERENCES chatrooms(chatroom_id) NOT NULL,
+    message_text VARCHAR (350) NOT NULL,
+    message_date DATE NOT NULL
 );
 
